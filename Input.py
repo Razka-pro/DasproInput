84039d8fa820ba44efe93a04ca644ee96c90ee03
Razka-pro <apaa10673@gmail.com>
2025-11-07 09:38:01 +0700

    Menambah fitur input

--- Input.py
+++ Input.py
@@ -1,2 +1,8 @@
 #Menginput Nama Pengguna
 print("Welcome to the program!")
+
+nama = str(input("Siapa nama kamu? "))
+umur = int(input("Berapa umur kamu? "))
+asal = str(input("Dimana asal kamu?"))
+
+print("selamat datang" , nama , "umur anda sekarang" , umur , "asal", asal)
